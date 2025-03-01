250951701ca62c804a8bdc14d1337d4168fea479
Muhammad Nour <acadamiclearner@gmail.com>
2025-03-01 11:09:22 +0200

    create wallet-server file structure

---
+++ wallet-server/index.php
@@ -0,0 +1,14 @@
+<?php
+
+function rev($num) {
+
+    $sign = $num < 0 ? -1: 1;
+    $res = 0;
+    while($num > 0) {
+        $res += 10*($num%10);
+        $num /= 10;
+    }
+    $res *= $sign;
+    return $res;
+
+}
