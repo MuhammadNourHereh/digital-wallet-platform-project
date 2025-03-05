72369f3421554a99c39e99f7007367e85b0c2afa
Muhammad Nour <acadamiclearner@gmail.com>
2025-03-05 20:59:54 +0200

    edit register.php to include balance id

--- wallet-server/connection/user/v1/api/register.php
+++ wallet-server/connection/user/v1/api/register.php
@@ -4,26 +4,28 @@ require_once path("conn");
 require_once path("cors-headers");
 
 if (!isset($_POST["username"]) || !isset($_POST["password"])) {
-
   http_response_code(400);
-
   echo json_encode([
     "message" => "username and password are required"
   ]);
-
   exit();
 }
 
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-
-// Validation for existing username
-
-$hashed = password_hash($password, PASSWORD_BCRYPT);
-$query = $conn->prepare("INSERT INTO users(username, password) values(?, ?)");
 try {
-  $query->bind_param("ss", $username, $hashed);
+  // create the balance entity
+  $query = $conn->prepare("INSERT INTO balance(amount_lb, amount_usd) values(0, 0)");
+  $query->execute();
+  $balanceId = $conn->insert_id;
+
+
+  // Validation for existing username
+  $hashed = password_hash($password, PASSWORD_BCRYPT);
+
+  $query = $conn->prepare("INSERT INTO users(username, password, balance_id) values(?, ?, ?)");
+  $query->bind_param("ssi", $username, $hashed, $balanceId);
   $query->execute();
   $query = $conn->prepare("SELECT * FROM users WHERE username = ?");
   $query->bind_param("s", $username);
