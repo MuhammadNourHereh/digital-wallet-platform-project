f85c0e109d97c6606bf051ea47afe291ebb9caa0
Muhammad Nour <acadamiclearner@gmail.com>
2025-03-05 16:07:54 +0200

    validate registeration input on the frontend

--- wallet-client/scripts/register.js
+++ wallet-client/scripts/register.js
@@ -1,13 +1,26 @@
+import { passwordVarify, usernameVarify } from './utils.js'
+
 const baseUrl = 'http://localhost:3000/'
 
 document.getElementById("loginForm").addEventListener("submit", function (e) {
     e.preventDefault();
     const form = new FormData(this)
 
+    if (!usernameVarify(form.get('username'))) {
+        console.log("username is not valid")
+        return
+    }
+
+    if (!passwordVarify(form.get('password'))) {
+        console.log("password is not valid")
+        return
+    }
+
     if (form.get('password') != form.get('repassword')) {
         console.log("password should match repassword")
         return
     }
+
     axios.post(`${baseUrl}connection/user/v1/api/register.php`,
         form
     )
